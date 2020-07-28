f80b6a5277622a24480b756551f2539bd1fb422d
karate78 <karatephotography@gmail.com>
2020-07-27 20:19:21 -0500

    Added Customer User Data

---
+++ ReservationProject.UI.MVC/Controllers/OwnerAssetsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ReservationProject.DATA.EF;
+
+namespace ReservationProject.UI.MVC.Controllers
+{
+    public class OwnerAssetsController : Controller
+    {
+        private SkatecampReservationsEntities db = new SkatecampReservationsEntities();
+
+        // GET: OwnerAssets
+        public ActionResult Index()
+        {
+            var ownerAssets = db.OwnerAssets.Include(o => o.UserDetail);
+            return View(ownerAssets.ToList());
+        }
+
+        // GET: OwnerAssets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OwnerAsset ownerAsset = db.OwnerAssets.Find(id);
+            if (ownerAsset == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ownerAsset);
+        }
+
+        // GET: OwnerAssets/Create
+        public ActionResult Create()
+        {
+            ViewBag.OwnerId = new SelectList(db.UserDetails, "UserId", "FirstName");
+            return View();
+        }
+
+        // POST: OwnerAssets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OwnerAssetId,ChildName,OwnerId,ChildPhoto,SpecialNotes,IsActive,DateAdded,ChildDOB,SkillLevel")] OwnerAsset ownerAsset)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OwnerAssets.Add(ownerAsset);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.OwnerId = new SelectList(db.UserDetails, "UserId", "FirstName", ownerAsset.OwnerId);
+            return View(ownerAsset);
+        }
+
+        // GET: OwnerAssets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OwnerAsset ownerAsset = db.OwnerAssets.Find(id);
+            if (ownerAsset == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OwnerId = new SelectList(db.UserDetails, "UserId", "FirstName", ownerAsset.OwnerId);
+            return View(ownerAsset);
+        }
+
+        // POST: OwnerAssets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OwnerAssetId,ChildName,OwnerId,ChildPhoto,SpecialNotes,IsActive,DateAdded,ChildDOB,SkillLevel")] OwnerAsset ownerAsset)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ownerAsset).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OwnerId = new SelectList(db.UserDetails, "UserId", "FirstName", ownerAsset.OwnerId);
+            return View(ownerAsset);
+        }
+
+        // GET: OwnerAssets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OwnerAsset ownerAsset = db.OwnerAssets.Find(id);
+            if (ownerAsset == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ownerAsset);
+        }
+
+        // POST: OwnerAssets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OwnerAsset ownerAsset = db.OwnerAssets.Find(id);
+            db.OwnerAssets.Remove(ownerAsset);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ReservationProject.UI.MVC/Controllers/ReservationsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ReservationProject.DATA.EF;
+
+namespace ReservationProject.UI.MVC.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private SkatecampReservationsEntities db = new SkatecampReservationsEntities();
+
+        // GET: Reservations
+        public ActionResult Index()
+        {
+            var reservations = db.Reservations.Include(r => r.Location).Include(r => r.OwnerAsset);
+            return View(reservations.ToList());
+        }
+
+        // GET: Reservations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservation);
+        }
+
+        // GET: Reservations/Create
+        public ActionResult Create()
+        {
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "LocationName");
+            ViewBag.OwnerAssetId = new SelectList(db.OwnerAssets, "OwnerAssetId", "ChildName");
+            return View();
+        }
+
+        // POST: Reservations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ReservationId,OwnerAssetId,LocationId,ReservationDate")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reservations.Add(reservation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "LocationName", reservation.LocationId);
+            ViewBag.OwnerAssetId = new SelectList(db.OwnerAssets, "OwnerAssetId", "ChildName", reservation.OwnerAssetId);
+            return View(reservation);
+        }
+
+        // GET: Reservations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "LocationName", reservation.LocationId);
+            ViewBag.OwnerAssetId = new SelectList(db.OwnerAssets, "OwnerAssetId", "ChildName", reservation.OwnerAssetId);
+            return View(reservation);
+        }
+
+        // POST: Reservations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ReservationId,OwnerAssetId,LocationId,ReservationDate")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reservation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "LocationName", reservation.LocationId);
+            ViewBag.OwnerAssetId = new SelectList(db.OwnerAssets, "OwnerAssetId", "ChildName", reservation.OwnerAssetId);
+            return View(reservation);
+        }
+
+        // GET: Reservations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservation);
+        }
+
+        // POST: Reservations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Reservation reservation = db.Reservations.Find(id);
+            db.Reservations.Remove(reservation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
